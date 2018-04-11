d92a61098694613dd982cbbb7d9bdd5a8b17a802
mayanje <jean.mayan@externe.e-i.com>
2018-04-11 17:39:22 +0200

    #869: Start Implementing an IProgramClassBuilder interface.

---
+++ TypeCobol/Compiler/CupParser/NodeBuilder/IProgramClassBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TypeCobol.Compiler.CodeElements;
+
+namespace TypeCobol.Compiler.CupParser.NodeBuilder
+{
+    /// <summary>
+    /// Interface of a Program class builder based
+    /// </summary>
+    public interface IProgramClassBuilder
+    {
+        /// <summary>
+        /// Start a Cobol Program
+        /// </summary>
+        /// <param name="programIdentification">The Program Identification Code Element</param>
+        /// <param name="libraryCopy">The Library Copy Code Element</param>
+        void StartCobolProgram(ProgramIdentification programIdentification, LibraryCopyCodeElement libraryCopy);
+
+        /// <summary>
+        /// End The current Cobol Program
+        /// </summary>
+        void EndCobolProgram();
+    }
+}
---
+++ TypeCobol/Compiler/CupParser/NodeBuilder/ProgramClassBuilder.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TypeCobol.Compiler.CodeElements;
+using TypeCobol.Compiler.CodeModel;
+using TypeCobol.Compiler.Nodes;
+using TypeCobol.Compiler.Parser;
+
+namespace TypeCobol.Compiler.CupParser.NodeBuilder
+{
+    /// <summary>
+    /// The Program Class Builder
+    /// </summary>
+    public class ProgramClassBuilder : IProgramClassBuilder
+    {
+        /// <summary>
+        /// Program object resulting of the visit the parse tree
+        /// </summary>
+        private Program Program { get; set; }
+        public SyntaxTree SyntaxTree { get; set; }
+
+        // Programs can be nested => track current programs being analyzed
+        private Stack<Program> programsStack = null;
+
+        private Program CurrentProgram
+        {
+            get { return programsStack.Peek(); }
+            set { programsStack.Push(value); }
+        }
+
+        /// <summary>Class object resulting of the visit the parse tree</summary>
+        public CodeModel.Class Class { get; private set; }
+
+        private readonly SymbolTable TableOfIntrisic = new SymbolTable(null, SymbolTable.Scope.Intrinsic);
+        private SymbolTable TableOfGlobals;
+        private SymbolTable TableOfNamespaces;
+
+        public SymbolTable CustomSymbols
+        {
+            private get { throw new InvalidOperationException(); }
+            set
+            {
+                if (value != null)
+                {
+                    SymbolTable intrinsicTable = value.GetTableFromScope(SymbolTable.Scope.Intrinsic);
+                    SymbolTable nameSpaceTable = value.GetTableFromScope(SymbolTable.Scope.Namespace);
+
+                    intrinsicTable.DataEntries.Values.ToList().ForEach(d => d.ForEach(da => da.SetFlag(Node.Flag.NodeIsIntrinsic, true)));
+                    intrinsicTable.Types.Values.ToList().ForEach(d => d.ForEach(da => da.SetFlag(Node.Flag.NodeIsIntrinsic, true)));
+                    intrinsicTable.Functions.Values.ToList().ForEach(d => d.ForEach(da => da.SetFlag(Node.Flag.NodeIsIntrinsic, true)));
+
+                    TableOfIntrisic.CopyAllDataEntries(intrinsicTable.DataEntries.Values);
+                    TableOfIntrisic.CopyAllTypes(intrinsicTable.Types);
+                    TableOfIntrisic.CopyAllFunctions(intrinsicTable.Functions, AccessModifier.Public);
+
+                    if (nameSpaceTable != null)
+                    {
+                        TableOfNamespaces = new SymbolTable(TableOfIntrisic, SymbolTable.Scope.Namespace); //Set TableOfNamespace with program if dependencies were given. (See CLI.cs runOnce2() LoadDependencies())
+                        TableOfNamespaces.CopyAllPrograms(nameSpaceTable.Programs.Values);
+                    }
+
+                }
+                // TODO#249: use a COPY for these
+                foreach (var type in DataType.BuiltInCustomTypes)
+                    TableOfIntrisic.AddType(DataType.CreateBuiltIn(type)); //Add default TypeCobol types BOOLEAN and DATE
+            }
+        }
+
+
+
+        public NodeDispatcher Dispatcher { get; internal set; }
+
+
+
+        public Node CurrentNode { get { return SyntaxTree.CurrentNode; } }
+
+        public Dictionary<CodeElement, Node> NodeCodeElementLinkers = new Dictionary<CodeElement, Node>();
+
+        public void StartCobolProgram(ProgramIdentification programIdentification, LibraryCopyCodeElement libraryCopy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndCobolProgram()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
